66e0a247b067828f71dd558aa3ed4c1270a12b65
Coreight98 <maxcha98@naver.com>
2023-07-11 00:51:35 +0900

    feat: 코드 리뷰 반영

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,53 +1,72 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { cloneElement } from "react";
+import { cloneElement, FC, ReactElement } from "react";
 
 import { BsListUl, BsBookmark, BsPerson } from "react-icons/bs";
 import { HiOutlineFire } from "react-icons/hi";
 
-const Footer_Map = [
+interface FooterMenuProps {
+  href: string;
+  name: string;
+  icon: ReactElement;
+  selected: boolean;
+}
+
+const FooterRouter = [
   {
-    link: "/list",
+    href: "/list",
     name: "방 목록",
     icon: <BsListUl />,
   },
   {
-    link: "/room",
+    href: "/room",
     name: "내 방",
     icon: <BsBookmark />,
   },
   {
-    link: "/rank",
+    href: "/rank",
     name: "순위",
     icon: <HiOutlineFire />,
   },
   {
-    link: "/myPage",
+    href: "/myPage",
     name: "내정보",
     icon: <BsPerson />,
   },
 ];
 
+const FooterMenu: FC<FooterMenuProps> = ({ href, name, icon, selected }) => {
+  const color = selected ? "#FF8787" : "black";
+
+  return (
+    <Link
+      href={href}
+      className="w-full flex flex-col items-center justify-center color-black"
+    >
+      {cloneElement(icon, { size: 20, color })}
+      <p className="text-black text-xs" style={{ color }}>
+        {name}
+      </p>
+    </Link>
+  );
+};
+
 export default function Footer() {
   const pathName = usePathname();
   const currentPath = pathName.split("/")[1];
 
-  const isSeleted = (path: string) =>
-    `/${currentPath}` === path ? "#FF8787" : "black";
+  const isSeleted = (path: string) => `/${currentPath}` === path;
 
   return (
     <div className="w-full h-[56px] fixed bottom-0 bg-white flex justify-around">
-      {Footer_Map.map(({ link, name, icon }) => (
-        <Link
-          key={link}
-          href={link}
-          className="w-full flex flex-col items-center justify-center color-black"
-        >
-          {cloneElement(icon, { size: 20, color: isSeleted(link) })}
-          <p className="text-black text-xs" style={{ color: isSeleted(link) }}>
-            {name}
-          </p>
-        </Link>
+      {FooterRouter.map(({ href, name, icon }) => (
+        <FooterMenu
+          key={href}
+          href={href}
+          name={name}
+          icon={icon}
+          selected={isSeleted(href)}
+        />
       ))}
     </div>
   );
